694182f387fa519db4ff09362c9e55c3197b0269
jrbarrio <jorge.roldan@gmail.com>
2021-02-20 17:54:59 +0100

    Updates

---
+++ precache-manifest.e149d3b7f61b7f0a9217fecb5ddf00da.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70af7476a53d12af950b99230b5c775f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e89cd6ff39d0812f63a4",
+    "url": "./static/css/main.1b9bcb54.chunk.css"
+  },
+  {
+    "revision": "6d2c1e9efc1a418bf8ab",
+    "url": "./static/js/2.7610e689.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.7610e689.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e89cd6ff39d0812f63a4",
+    "url": "./static/js/main.cfbb498f.chunk.js"
+  },
+  {
+    "revision": "82d77bf472a1871bbaf6",
+    "url": "./static/js/runtime-main.b67f9ccd.js"
+  }
+]);
